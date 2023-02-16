273ec6cc1aebf456cbe3839ee36f1be140cce9ed
sproshchaev <sproshchaev@gmail.com>
2023-02-16 17:26:31 +0500

    Updated

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # geotools-demo
 
-Manual: https://www.baeldung.com/geo-tools
-
+### Repo:
 Url-repository osgeo, opengeo: https://repo.osgeo.org/repository/release
-
 MVN Repository: https://mvnrepository.com/artifact/org.geotools/gt-shapefile
                 https://mvnrepository.com/artifact/org.geotools/gt-epsg-hsql
+
+### Manual: 
+https://www.baeldung.com/geo-tools
+https://docs.geotools.org/stable/userguide/library/data/shape.html
--- src/main/java/com/prosoft/Main.java
+++ src/main/java/com/prosoft/Main.java
@@ -1,11 +1,40 @@
 package com.prosoft;
 
+import org.geotools.data.*;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * geotools-demo
- *
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException, SchemaException {
+
+        // Creating
+        FileDataStoreFactorySpi factory = new ShapefileDataStoreFactory();
+        File file = new File("my.shp");
+        Map<String, ?> map = Collections.singletonMap("url", file.toURI().toURL());
+        DataStore myData = factory.createNewDataStore(map);
+        SimpleFeatureType featureType = DataUtilities.createType("my", "geom:Point,name:String,age:Integer,description:String");
+        myData.createSchema(featureType);
+
+
+
+        System.out.println("Done!");
     }
 }
